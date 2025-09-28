c946cc16013ca2125064b504b7cae0a826d19aad
SKBWEBDEV <najmujshakib025@gmail.com>
2025-09-28 20:04:37 +0600

    navber responsive

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,17 +1,22 @@
 import Container from "../Layouts/Container"
 import { PiSunHorizonFill } from "react-icons/pi";
 import { MdOutlineWatchLater } from "react-icons/md";
-import { FaFacebookF } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
-import { AiFillInstagram } from "react-icons/ai";
-import { FaYoutube } from "react-icons/fa";
+// import { FaFacebookF } from "react-icons/fa";
+// import { FaTwitter } from "react-icons/fa";
+// import { AiFillInstagram } from "react-icons/ai";
+// import { FaYoutube } from "react-icons/fa";
+
+import Facebook from "../../assets/facebook.mp4"
+import Instagram from "../../assets/instagram.mp4"
+import Twiter from "../../assets/twiter.mp4"
+import Youtube from "../../assets/youtube.mp4"
 
 const Header = () => {
   return (
     <div className="bg-cyan-500 shadow-lg shadow-cyan-500/50 text-white font-bold py-5">
       <Container>
 
-       <div className="md:flex justify-between ">
+       <div className=" md:flex md:items-center md:justify-between ">
          <div className="flex items-center gap-[10px]">
           <p><PiSunHorizonFill /> </p>
           <span>15 <sup>0C</sup></span>
@@ -20,17 +25,34 @@ const Header = () => {
 
         <div className="md:flex  items-center gap-[15px]">
           <p className="md:block hidden"><MdOutlineWatchLater /></p>
-          <p className="mt-5">Wednesday, 8 January 2025</p>
-          <p className="bg-[#F65050] py-[14px] w-fit px-[14px] text-white mt-5">Breaking News</p>
-          <p className="mt-5">Indonesia says located black box recorders from crashed plane</p>
+          <p className="mt-5 md:mt-0">Wednesday, 8 January 2025</p>
+          <p className="bg-[#F65050] py-[14px] w-fit px-[14px] text-white mt-5 md:mt-0">Breaking News</p>
+          <p className="mt-5 md:mt-0">Indonesia says located black box recorders from crashed plane</p>
         </div>
 
-        <div  className="flex  items-center gap-5 mt-5">
+        {/* <div  className="flex mt-5 md:mt-0 items-center gap-5 ">
           <p className="bg-[#1B7BFD] py-[8px] px-[9px] rounded-full text-white"><FaFacebookF /></p>
           <p className="bg-[#42C0F5] py-[8px] px-[9px] rounded-full text-white"><FaTwitter /></p>
           <p className="bg-[#C23785] py-[8px] px-[9px] rounded-full text-white"><AiFillInstagram  /></p>
           <p className="bg-[#EF5043] py-[8px] px-[9px] rounded-full text-white"><FaYoutube  /></p>
+        </div> */}
+        <div>
+          <ul   className="flex mt-5 md:mt-0 items-center gap-5 ">
+            <li>
+             <video src={Facebook} autoPlay loop muted playsInline className="w-10 h-10 rounded-full"/>
+            </li>
+            <li>
+             <video src={Instagram} autoPlay loop muted playsInline className="w-10 h-10 rounded-full"/>
+            </li>
+            <li>
+             <video src={Twiter} autoPlay loop muted playsInline className="w-10 h-10 rounded-full"/>
+            </li>
+            <li>
+             <video src={Youtube} autoPlay loop muted playsInline className="w-10 h-10 rounded-full"/>
+            </li>
+          </ul>
         </div>
+
        </div>
 
       </Container>
--- src/Components/Navber/Navber.jsx
+++ src/Components/Navber/Navber.jsx
@@ -12,20 +12,20 @@ const Navber = () => {
       <Container>
         <div className="bg-[#393939] md:block hidden">
           <ul className="md:flex gap-[25px] font-bold text-white">
-            <li className="py-[15px] px-[17px] hover:bg-[#F65050]  hover:underline transition hover:h-full"> <a href="">Home</a></li>
-            <li className="py-[15px] px-[17px] hover:bg-[#F65050]  hover:underline transition hover:h-full"> <a href="">International</a></li>
-            <li className="py-[15px] px-[17px] hover:bg-[#F65050]  hover:underline transition hover:h-full"> <a href="">Sports</a></li>
-            <li className="py-[15px] px-[17px] hover:bg-[#F65050]  hover:underline transition hover:h-full"> <a href="">Opinion</a></li>
-            <li className="py-[15px] px-[17px] hover:bg-[#F65050]  hover:underline transition hover:h-full"> <a href="">Business</a></li>
-            <li className="py-[15px] px-[17px] hover:bg-[#F65050]  hover:underline transition hover:h-full"> <a href="">YouthEntertainment</a></li>
-            <li className="py-[15px] px-[17px] hover:bg-[#F65050]  hover:underline transition hover:h-full"> <a href="">Lifestyle</a></li>
-            <li className="flex items-center py-[15px] px-[15px] hover:bg-[#F65050]  hover:underline transition "> <a href="">Pages </a><span><HiChevronDown /></span></li>
+            <li className="py-[15px] px-[17px] hover:bg-[#F65050]  tr"> <a href="">Home</a></li>
+            <li className="py-[15px] px-[17px] hover:bg-[#F65050]  transition "> <a href="">International</a></li>
+            <li className="py-[15px] px-[17px] hover:bg-[#F65050]  transition "> <a href="">Sports</a></li>
+            <li className="py-[15px] px-[17px] hover:bg-[#F65050]  transition "> <a href="">Opinion</a></li>
+            <li className="py-[15px] px-[17px] hover:bg-[#F65050]  transition "> <a href="">Business</a></li>
+            <li className="py-[15px] px-[17px] hover:bg-[#F65050]  transition "> <a href="">YouthEntertainment</a></li>
+            <li className="py-[15px] px-[17px] hover:bg-[#F65050]  transition "> <a href="">Lifestyle</a></li>
+            <li className="flex items-center py-[15px] px-[15px] hover:bg-[#F65050]  transition "> <a href="">Pages </a><span><HiChevronDown /></span></li>
 
-            <li className="mt-5 md:mt-0 py-5 ml-5 flex items-center gap-5 border-l-4 text-white/30"><a href="">Search for</a><a href=""><span><FaSearch /></span></a></li>
+            <li className="mt-5 md:mt-0 py-5 ml-5 flex items-center gap-5 border-l-4 text-white/30"><a className="ml-10" href="">Search for</a><a href=""><span><FaSearch /></span></a></li>
           </ul>
         </div>
 
-        <div className="md:hidden block">
+        <div className="md:hidden  flex justify-between">
           {
             show ?  <div className="bg-[#393939] ">
           <ul className="md:flex gap-[25px] font-bold text-white">
@@ -38,7 +38,7 @@ const Navber = () => {
             <li className="py-[15px] px-[17px] hover:bg-[#F65050]  hover:underline transition hover:h-full"> <a href="">Lifestyle</a></li>
             <li className="flex items-center py-[15px] px-[15px] hover:bg-[#F65050]  hover:underline transition "> <a href="">Pages </a><span><HiChevronDown /></span></li>
 
-            <li className="mt-5 md:mt-0 py-5 ml-5 flex items-center gap-5 border-l-4 text-white/30"><a href="">Search for</a><a href=""><span><FaSearch /></span></a></li>
+            <li className="mt-5 md:mt-0 py-5 ml-5 flex items-center gap-5"><a href="">Search for</a><a href=""><span><FaSearch /></span></a></li>
           </ul>
         </div>: ""
           }
